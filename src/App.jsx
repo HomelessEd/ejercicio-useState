12478cf7a5c64dd4e102b20b079b9d37b57c158a
Edward Anderson <edward.a93@hotmail.co.uk>
2026-02-23 18:37:08 +0100

    App created and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
+import { useState } from 'react'
 import './App.css'
 
+
 function App() {
- //Aquí tu código
+ const [teachers, setTeachers] = useState(['Data', 'Reyes', 'Yolanda']);
+ const [name, setName] = useState('Sofia');
+ const [newName, setNewName]= useState('');
+ const changeName = (e) => {e.preventDefault();
+     if(newName.trim() !== ''){
+        setName(newName);
+        setTeachers([...teachers,newName])
+        setNewName('');
+      }
+};
+
+ const deleteTeacher = (indextoDelete) => {
+ const sweepTeachers = teachers.filter((_ , index) => index !== indextoDelete);
+ setTeachers(sweepTeachers)
+}; return (
+    <div>
+        <h2>Teacher Name: {name}</h2>
+        <ul>
+            {teachers.map ((t, index) => (
+               <li key={index}> 
+    <button onClick={() => setName(t)}>{t}</button>
+    <button onClick={() => deleteTeacher(index)}>Delete</button>
+  </li>
+            ))}
+        </ul>
+        <form onSubmit={changeName}>
+  <input 
+    type="text" 
+    placeholder="add a name" 
+    value={newName} 
+    onChange={(e) => setNewName(e.target.value)} 
+  />
+  <button type="submit">Add</button>
+</form>
+    </div>
+  );
 }
 
 export default App
